c1134bb4c0c98d6bd4c9ae5ade7aa0efe93be536
Przemysław Świderski <przemekzgierz7@gmail.com>
2023-08-25 15:07:32 +0200

    Update Gradle to 8.3 and other related dependencies. (#17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.changelog.date
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlinx.kover") version "0.5.1"
-    id("com.gradle.plugin-publish") version "1.0.0"
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.kotlinx.kover") version "0.7.3"
+    id("com.gradle.plugin-publish") version "1.2.1"
     id("net.researchgate.release") version "2.8.1"
     id("org.jetbrains.changelog") version "1.3.1"
 }
@@ -19,7 +20,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.35.0")
 }
 
 kotlin {
@@ -33,7 +34,7 @@ tasks {
         useJUnitPlatform()
         testLogging.showStandardStreams = true
     }
-    "afterReleaseBuild" {
+    afterReleaseBuild {
         dependsOn(
             "publish",
             "publishPlugins",
@@ -41,39 +42,39 @@ tasks {
         )
     }
     compileKotlin {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-}
-gradlePlugin {
-    plugins {
-        create("python-gradle-plugin") {
-            id = "com.pswidersk.python-plugin"
-            implementationClass = "com.pswidersk.gradle.python.PythonPlugin"
-            displayName =
-                "Gradle plugin to run Python projects in Conda virtual env. https://github.com/PrzemyslawSwiderski/python-gradle-plugin"
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
         }
     }
 }
 
-pluginBundle {
-    website = "https://github.com/PrzemyslawSwiderski/python-gradle-plugin"
-    vcsUrl = "https://github.com/PrzemyslawSwiderski/python-gradle-plugin"
-    description = "Gradle plugin to run Python projects."
-    tags = listOf(
-        "python",
-        "venv",
-        "numpy",
-        "conda",
-        "miniconda",
-        "anaconda",
-        "scipy",
-        "pandas",
-        "flask",
-        "matplotlib",
-        "sklearn"
-    )
+gradlePlugin {
+    website.set("https://github.com/PrzemyslawSwiderski/python-gradle-plugin")
+    vcsUrl.set("https://github.com/PrzemyslawSwiderski/python-gradle-plugin")
+    plugins {
+        create("python-gradle-plugin") {
+            id = "com.pswidersk.python-plugin"
+            implementationClass = "com.pswidersk.gradle.python.PythonPlugin"
+            displayName = "Gradle plugin to run Python projects in Conda virtual env. "
+                .plus("https://github.com/PrzemyslawSwiderski/python-gradle-plugin")
+            description = "Gradle plugin to run Python projects."
+            tags.set(
+                listOf(
+                    "python",
+                    "venv",
+                    "numpy",
+                    "conda",
+                    "miniconda",
+                    "anaconda",
+                    "scipy",
+                    "pandas",
+                    "flask",
+                    "matplotlib",
+                    "sklearn"
+                )
+            )
+        }
+    }
 }
 
 publishing {
